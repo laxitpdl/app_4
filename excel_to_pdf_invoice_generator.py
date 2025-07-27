28c61db69188cf79e59fcb1ee77247f893680df8
laxitpdl <poudellakshit042061@gmail.com>
2025-07-27 14:08:10 -0500

    pdf

--- excel_to_pdf_invoice_generator.py
+++ excel_to_pdf_invoice_generator.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import glob
+from fpdf import FPDF 
+from pathlib import Path
+import os
+os.makedirs("PDFs", exist_ok=True)
 
 
 filepaths = glob.glob("app_4/*.xlsx")
@@ -7,3 +11,13 @@ filepaths = glob.glob("app_4/*.xlsx")
 for filepath in filepaths:
           df = pd.read_excel(filepath, sheet_name="Sheet 1")
           print (df)
+          pdf = FPDF(orientation = "P", unit = "mm", format = "A4")
+          pdf.add_page()
+          filename = Path(filepath).stem
+          invoice_nr = filename.split('-')[0]
+          pdf.set_font(family="Times", size=16, style = "B")
+          pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}", ln=1)
+          pdf.output(f'PDFs/{filename}.pdf')
+          
+
+
